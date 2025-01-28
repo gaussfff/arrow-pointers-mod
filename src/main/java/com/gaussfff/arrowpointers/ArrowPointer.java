b8fd685a398b93f6f2164b47aa479aa05c8470ab
Bohdan Sokolovskyi <sokol.chemist@gmail.com>
2025-01-28 16:36:42 +0200

    init project

---
+++ src/main/java/com/gaussfff/arrowpointers/ArrowPointer.java
@@ -0,0 +1,10 @@
+package com.gaussfff.arrowpointers;
+
+import net.minecraft.world.item.SignItem;
+
+public class ArrowPointer extends SignItem {
+
+    public ArrowPointer() {
+        super();
+    }
+}
